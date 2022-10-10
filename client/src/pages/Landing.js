8236ba6c18e08da3898dad237d163635922546cf
Moni <moni@Monis-MacBook-Pro.local>
2022-10-10 12:09:22 +0300

    Full workable app from local mechine server

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,37 @@
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
+import { Link } from 'react-router-dom'
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+                        Prioritize wisely
+                        <sup>Time & Task</sup>
+                    </p>
+          <Link to='/register' className='btn btn-hero'
+          style={{
+            fontSize: '3vmin',
+            background: 'linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 55%, rgba(252,176,69,1) 100%)'
+            
+        }}
+          >
+          Login/SignUp
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  )
+}
+
+export default Landing
